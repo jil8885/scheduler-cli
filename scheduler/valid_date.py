45a88a56146699c07eef2a5155a2bb6b94d7177a
Minjun Kim <31723018+KmMnJn@users.noreply.github.com>
2018-06-08 23:37:42 +0900

    Update valid_date.py

--- scheduler/valid_date.py
+++ scheduler/valid_date.py
@@ -3,4 +3,4 @@ def valid_date(year, month, day):
 	mth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 	if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
 		mth[1] = 29
-	return 0 < day and day<mth[month-1]
+	return month < 12 and 0 < day and day <= mth[month-1]
